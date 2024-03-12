c6704e24923304a8f41c41f1c82ecd20002d93b9
Sergiy Horodetskiy <bryklin.best.1994@gmail.com>
2024-03-12 17:53:55 +0200

    initial-code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import cinemaApi from './cinema-api';
 
 function App() {
-    // const [count, setCount] = useState(0)
+    // const [count, setCount] = useState();
+
+    useEffect(() => {
+        async function getCinema() {
+            try {
+                const data = await cinemaApi();
+                console.log(data);
+                // setArrPhoto(oldPhoto => [...oldPhoto, ...data.results]);
+                // setTotalPages(data.total_pages);
+            } catch (error) {
+                console.log(error);
+                // setError(true);
+            } finally {
+                // setLoading(false);
+            }
+        }
+        getCinema();
+    }, []);
 
     return <></>;
 }
